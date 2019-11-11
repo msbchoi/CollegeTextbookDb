e672956a8ac6922e263b40e9a4d97ff9d2c02cd7
cocozhangyuke <cocozhangyuke@users.noreply.github.com>
2019-11-11 18:50:05 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" />
+  <link rel="shortcut icon" type="image/png" href="{%  static 'favicon.ico' %}"/>
+  <link rel="canonical" href="https://getbootstrap.com/docs/3.3/examples/carousel/">
+  <meta name="author" content="">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Login</title>  
+  <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/static/skillshare/carousel.css">
+    <link rel="icon" href="/static/skillshare/img/fav.png" type="image/x-icon">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" />
-  <meta name="author" content="Matt Vick, Yoon Kim">
+  <meta name="author" content="">
   <title>Login</title>    
 </head>
 
@@ -43,5 +60,20 @@
 	  <input type="submit" value="Sign in" class="btn btn-light"  />   
     </form>
   </div>
+  <!-- FOOTER -->
+  <footer style="color:#1e1e1e;">
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; We Rock </p>
+      </footer>
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
