b4002cbeb017755c1803b7e1d19f809c2a74df7c
cocozhangyuke <cocozhangyuke@users.noreply.github.com>
2019-11-15 17:37:20 -0500

    Update export.php

--- export.php
+++ export.php
@@ -1,48 +1,53 @@
 <?php
+
+session_start();
+require "dbutil.php";
 $db = DbUtil::loginConnection($_SESSION['level'], $_SESSION['levelpwd']);
-$output = '';
-if(isset($_POST['search']))
-{
-	$valueToSearch = $_POST['ba;ueToSearch'];
-	$query = "SELECT * FROM 'P_COURSES' WHERE CONCAT('course_name','course_id','number_of_credits') LIKE '%". $valueToSearch."%'";
-	$search_result = filterTable($query);
-}
-else{
-	$query="SELECT * FROM 'course_id'";
-	$search_result = filterTable($query);
-}
-function filterTable($query)
-{
-	$filter_Result = mysqli_query($db, $query);
-	return $filter_Result;
-}
-if(isset($_POST["export"]))
-{
- $result = $search_result;
- if(mysqli_num_rows($result) > 0)
- {
-  $output .= '
-   <table class="table" bordered="1">  
-                    <tr>  
-                         <th>course_name</th>  
-                         <th>course_id</th>  
-                         <th>Credits</th>  
-                    </tr>
-  ';
-  while($row = mysqli_fetch_array($result))
-  {
-   $output .= '
-    <tr>  
-                         <td>'.$row["course_name"].'</td>  
-                         <td>'.$row["course_id"].'</td>  
-                         <td>'.$row["number_of_credits"].'</td>  
-                    </tr>
-   ';
-  }
-  $output .= '</table>';
-  header('Content-Type: application/xls');
-  header('Content-Disposition: attachment; filename=download.xls');
-  echo $output;
- }
-}
+echo "<p>Logged in</p>";
+$sql = "SELECT * FROM p_courses";
+  $result = $db->query("SELECT * FROM P_COURSES") ;
 ?>
+
+<!DOCTYPE html>
+<html>  
+ <head>  
+  <title>Export MySQL data to Excel in PHP</title>  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+ </head>  
+ <body>  
+  <div class="container">  
+   <br />  
+   <br />  
+   <br />  
+   <div class="table-responsive">  
+    <h2 align="center">Export MySQL data to Excel in PHP</h2><br /> 
+    <table class="table table-bordered">
+     <tr>  
+                         <th>course_name</th>
+                    <th>course_id</th>
+                    <th>credits</th>
+                    </tr>
+     <?php
+     while($row = mysqli_fetch_array($result))  
+     {  
+        echo '  
+       <tr>  
+         <td>'.$row["course_name"].'</td>  
+         <td>'.$row["course_id"].'</td>  
+         <td>'.$row["number_of_credits"].'</td>  
+       </tr>  
+        ';  
+     }
+     ?>
+    </table>
+
+    <br />
+    <form method="post" action="export_page.php">
+     <input type="submit" name="export" class="btn btn-success" value="Export" />
+    </form>
+   </div>  
+  </div>  
+ </body>  
+</html>
