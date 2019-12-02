5e7bc1799b9bc8a94a7a166460c7a626376ab9a0
msbchoi <msbchoi@gmail.com>
2019-12-01 19:00:30 -0500

    modifications to department

--- add_department.php
+++ add_department.php
@@ -18,8 +18,24 @@
         <input type = "text" name = "department_name" size = 40/></br>
         <input type="submit" value="Add" name="action" />
     </form>
+<?php
 
+$stmt = $db->stmt_init();
+if($stmt->prepare("select * from P_DEPARTMENT") or die(mysqli_error($db))) {
+        $stmt->execute();
+        $stmt->bind_result($department_id, $department_name);
+        echo "<table border=1><th>Department Name</th><th>Department Id</th></th>\n";
+        while($stmt->fetch()) {
+                echo "<tr><td>$department_name</td><td>$department_id</td></tr>";
+        }
+        echo "</table>";
+
+        $stmt->close();
+}
+?>
 </body>
+
+
 
 
 </html>
