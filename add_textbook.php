6e6ffd2af21601c1db68863f86be54003b0f69fe
cocozhangyuke <cocozhangyuke@users.noreply.github.com>
2019-12-02 23:49:50 -0500

    Update add_textbook.php

--- add_textbook.php
+++ add_textbook.php
@@ -2,17 +2,17 @@
 <html>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="Minsoo Choi, Matt Vick, Shen Yan, Coco Zhang">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/themify-icons.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/theme-nearblack.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
+
 <title>Add a Textbook</title>
-<script src=" 
-https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> 
-    </script> 
-
-
-
-    <script>
-     //src = "jquery-3.4.1.min"
-      </script>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
 <script>
         $(document).ready(function(){ 
             $("#addauthor").click(function(){ 
@@ -27,8 +27,20 @@ https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
 </script>
 </head>
 <body>
+<section class="page-title page-title-1 bg-secondary">
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-12 text-center">
+		                    <h2 class="uppercase mb0">Add textbooks</h2>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		        
+ </section>
 <button onclick="window.location.href='main_page.php'"> Return to Main Page</button>
-
+<!-- domain of adding textbooks -->
 <?php
 
     session_start();
@@ -72,7 +84,28 @@ https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
     <button id ="addauthor"> Add another author </button> </br>
     
     <div>
+    <footer class="footer-2 bg-dark text-center-xs">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4">
+							<a href="#"><img alt="Logo" class="image-mediem mb8" src="img/logo-white.png"></a>
+						</div>
+					
+						<div class="col-sm-4 text-center">
+							<span class="fade-half">
+								© Copyright 2019 NOLIFE TB - All Rights Reserved
+							</span>
+						</div>
+					
+						
+					</div>
+				</div>
+			</footer>
     </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/parallax.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 
 
